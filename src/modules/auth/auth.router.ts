7bfd50683dfed52378b40917acc9729a0b9a06cb
Prihandika_ <139831061+prhndk123@users.noreply.github.com>
2026-02-04 10:26:15 +0700

    feat: Implement core authentication module with registration, login, Google OAuth, DTOs, services, routes, and supporting validation and error handling middleware. (#4)

--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -1,16 +1,35 @@
+import { ValidationMiddleware } from "../../middleware/validation.middleware.js";
 import { AuthController } from "./auth.controller.js";
 import express, { Router } from "express";
+import { RegisterDto } from "./dto/register.dto.js";
+import { LoginDto } from "./dto/login.dto.js";
+import { GoogleDto } from "./dto/google.dto.js";
 
 export class AuthRouter {
   private router: Router;
-  constructor(private authController: AuthController) {
+  constructor(
+    private authController: AuthController,
+    private validationMiddleware: ValidationMiddleware,
+  ) {
     this.router = express.Router();
     this.initRoutes();
   }
   private initRoutes = () => {
-    this.router.post("/register", this.authController.register);
-    this.router.post("/login", this.authController.login);
-    this.router.post("/google", this.authController.google);
+    this.router.post(
+      "/register",
+      this.validationMiddleware.validateBody(RegisterDto),
+      this.authController.register,
+    );
+    this.router.post(
+      "/login",
+      this.validationMiddleware.validateBody(LoginDto),
+      this.authController.login,
+    );
+    this.router.post(
+      "/google",
+      this.validationMiddleware.validateBody(GoogleDto),
+      this.authController.google,
+    );
   };
   getRouter = () => {
     return this.router;
