a5d29521781a820cae663525820c7b4c0a46019e
prhndk123 <prihandikavahryansyah@gmail.com>
2026-02-05 11:29:19 +0700

    feat: Implement initial API setup including authentication controller, middleware, cookie parsing, and CORS configuration.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AuthService } from "./auth.service.js";
+import { cookieOptions } from "../../config/cookie.js";
 
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -12,7 +13,9 @@ export class AuthController {
   login = async (req: Request, res: Response) => {
     const body = req.body;
     const result = await this.authService.login(body);
-    res.status(200).send(result);
+    res.cookie("accessToken", result.accessToken, cookieOptions);
+    const { accessToken, ...userWithoutAccessToken } = result;
+    res.status(200).send(userWithoutAccessToken);
   };
 
   google = async (req: Request, res: Response) => {
