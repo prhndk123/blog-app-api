4fc200dc34670b3f164c413b42b67c8aa44cfc29
prhndk123 <prihandikavahryansyah@gmail.com>
2026-02-13 10:18:32 +0700

    feat: add Redis connection configuration.

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,6 +1,8 @@
 import { Redis } from "ioredis";
 
 export const connection = new Redis({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
   host: process.env.REDIS_HOST || "localhost",
   port: Number(process.env.REDIS_PORT) || 6379,
 });
